708f428914c25973f4a6c5e661f12b251e8a7bac
Kimo-Store <Kimo Store@DESKTOP-RTPEEP9>
2025-02-25 21:16:53 +0200

    q1

---
+++ Assignment/Program.cs
@@ -0,0 +1,18 @@
+﻿using static Assignment.ListGenerators;
+namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region q1 : Get first Product out of Stock 
+
+            //var result=ProductList.First(x=>x.UnitsInStock==0);
+            //Console.WriteLine(result);
+
+            #endregion
+
+            
+        }
+    }
+}
